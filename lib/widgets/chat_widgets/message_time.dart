d2abc706cbaf66a36eca7bf08e68692097e990d6
uzany <uzan.yakir@gmail.com>
2024-11-18 15:39:17 +0200

    changed chat structure

--- lib/widgets/chat_widgets/message_time.dart
+++ lib/widgets/chat_widgets/message_time.dart
@@ -4,7 +4,7 @@ import '../../models/chat.dart';
 import '../../models/message.dart';
 import '../../utils.dart';
 
-class MessageTime extends StatefulWidget {
+class MessageTime extends StatelessWidget {
   final Chat chat;
   final Message message;
   final bool isMe;
@@ -14,50 +14,24 @@ class MessageTime extends StatefulWidget {
       {super.key, required this.chat, required this.message, required this.isMe, required this.messageReadNotifier});
 
   @override
-  State<MessageTime> createState() => _MessageTimeState();
-}
-
-class _MessageTimeState extends State<MessageTime> {
-  late bool _messageRead = false;
-
-  void _onMessageReadChanged() {
-    setState(() {
-      _messageRead = !widget.messageReadNotifier.lastMessageSeenTime.isBefore(widget.message.sentAt);
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    if (widget.isMe) {
-      _onMessageReadChanged();
-      widget.messageReadNotifier.addListener(_onMessageReadChanged);
-    }
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-
-    if (widget.isMe) {
-      widget.messageReadNotifier.removeListener(_onMessageReadChanged);
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        if (widget.isMe)
-          Icon(_messageRead ? Icons.done_all : Icons.done,
-              color: _messageRead ? Colors.cyan[300] : Colors.grey[300], size: 18),
+        if (isMe)
+          ListenableBuilder(
+            listenable: messageReadNotifier,
+            builder: (context, child) {
+              bool messageRead = !messageReadNotifier.lastMessageSeenTime.isBefore(message.sentAt);
+              return Icon(messageRead ? Icons.done_all : Icons.done,
+                  color: messageRead ? Colors.cyan[300] : Colors.grey[300], size: 18);
+            },
+          ),
         const SizedBox(width: 4),
         Text(
-          getHourMinuteFormat(widget.message.sentAt),
+          getHourMinuteFormat(message.sentAt),
           style: TextStyle(
-            color: Colors.grey[widget.isMe ? 300 : 700],
+            color: Colors.grey[isMe ? 300 : 700],
             fontSize: 12,
           ),
         ),
